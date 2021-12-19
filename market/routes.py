3d6cc729bc63d62e11fe028c44dc5dedfe7e5b61
danielcristho <pepuhodaniel93.learn@gmail.com>
2021-12-19 20:20:40 +0900

    update home page

--- market/routes.py
+++ market/routes.py
@@ -1,9 +1,10 @@
+from logging import info
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import LoginForm, RegisterForms
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 @app.route('/')
 @app.route('/home')
@@ -47,3 +48,9 @@ def login_page():
             flash('Username and Password are not match! Please try again', category='danger')    
 
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for('home_page'))    
